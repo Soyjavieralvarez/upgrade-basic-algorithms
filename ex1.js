de4b3a7018e1e5291624068d61385d31d421c208
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-01-03 20:07:36 +0100

    condicionales

--- ex1.js
+++ ex1.js
@@ -31,7 +31,51 @@ globalBasePrice = 25000;
 
 const car1 = {name: 'BMW m&m', basePrice:50000, finalPrice:60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice:70000, finalPrice:80000};
-console.log(car1.finalPrice = car1.basePrice + globalBasePrice)
-console.log(car1.finalPrice = car2.basePrice + globalBasePrice)
+console.log(car1.finalPrice = car1.basePrice + globalBasePrice);
+console.log(car1.finalPrice = car2.basePrice + globalBasePrice);
+
+//! Interacción 3
+
+let multiplicar = 10*5;
+console.log(multiplicar);
+
+let dividir = 10/2;
+console.log(dividir);
+
+let resto = 15/9;
+console.log(resto);
+
+
+let x1 = (h+y);
+console.log(x1);
+
+let x2 = (h*y);
+console.log(x2);
+
+//! Interacción 4
+
+const avengers = ['Hulk', 'Spiderman', 'Black Panther'];
+console.log(avengers[0]);
+
+avengers.splice(0,1, 'IronMan');
+console.log(avengers);
+
+const avengers1 = ['Hulk', 'Spiderman', 'Black Panter']
+console.log(avengers1.length);
+
+const rickAndMortyCharacters = ['Rick', 'Beth', 'Jerry'];
+rickAndMortyCharacters.push('Morty', 'Summer')
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length -1]);
+
+const rickAndMortyCharacters1 = ['Rick', 'Beth', 'Jerry', 'Morty', 'Summer'];
+rickAndMortyCharacters1.pop;
+console.log(rickAndMortyCharacters1[0], rickAndMortyCharacters1[rickAndMortyCharacters1.length - 1]);
+
+const rickAndMortyCharacters2 = ['Rick', 'Beth', 'Jerry', 'Morty', 'Summer'];
+rickAndMortyCharacters2.splice(1,1)
+console.log(rickAndMortyCharacters2);
+
+//!Condicionales
+
 
 
